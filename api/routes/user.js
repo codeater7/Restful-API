61f9e6fc915004b6814a5fd78e6bd808d347d7a0
Sujan <pokharelsujan134@gmail.com>
2019-12-19 18:32:06 +0200

    issue: bcrypt comapare does not get tue for the same hashed value

--- api/routes/user.js
+++ api/routes/user.js
@@ -3,8 +3,13 @@ const router = express.Router();
 const mongoose = require('mongoose');
 
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
 
 const User = require('../models/user');
+
+// Json Wen token 
+// it will do token generation and signing
+
 
 // Create a new user using the post Route
 // overall the user will be user/signup
@@ -46,8 +51,9 @@ router.post('/signup', (req, res, next) => {
         // We do we get mail exists even if the mail does not exists?? 
         // In above case it is never going to be null so, it says mail exists
         // instead we need to check the length
+        // below user wull be the array we get
         .then(user =>{
-            console.log(user)
+            
             if (user.length >=1){
                 return res.status(409).json({ message: 'Mail exists'})
             }else{
@@ -76,11 +82,49 @@ router.post('/signup', (req, res, next) => {
         })
     })
 
-router.post('/login', (req, res, next)=>{
-    User.find({email:req.body.email})
+router.post("/login", (req, res, next)=>{
+    console.log(req.body.password)
+    User.find({ email:req.body.email })
         .exec()
-        .then()
-        .catch()
+        // users will be empty array
+        // finding the user
+        .then( user =>{
+            
+            
+            if (user.length <1){
+                
+               
+               // return res.status(404).json({ message: 'mail does not found, user does not exist'}) // brute force  so lets not do
+               return res.status(401).json({message:'Auth Failed'})
+            }
+            //we see if the password that is  sent as the request,  matches the password in our database
+            // documentation
+            bcrypt.compare(req.body.password, user[0].password, (err,result) =>{
+                if(err){
+                    console.log("comes here")
+                    return res.status(401).json({ message:'Auth failed'})}
+                if (result){
+                   
+                  
+                    return res.status(200).json({message: "Auth Successful"})
+
+                    // // See documentation
+                    // const token= jwt.sign({email:user[0].email, userId:user[0]._id},
+                    //           process.env.JWT_KEY, {expiresIn:"1hr"} );
+                    // return res.status(200).json({
+                    //     message:'Auth Successful',
+                    //     token:token})
+                    
+                }
+            
+                
+                res.status(401).json({message:'Auth failed'})
+            })
+        })
+        .catch(err =>{ 
+              
+            res.status(500).json({ error:err})
+        })
 
 })
 
